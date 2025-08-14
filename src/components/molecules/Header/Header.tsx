f86580b1655bb8909c2f9b54c46960b1c5f57264
Mikhail Adutskevich <mikhail.adutskevich@itm20304-2.home>
2025-08-14 16:51:40 +0200

    update: images for expo snack

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,16 +1,23 @@
 import { Image } from 'expo-image';
 import { Text, View } from 'react-native';
-import { LadyImage, LottoLogoImage } from '../../../assets';
 import { styles } from './Header.styled';
 import type { THeaderProps } from './Header.types';
 
 export const Header = ({ title }: THeaderProps) => (
   <View style={styles.container}>
-    <Image style={styles.personImage} source={LadyImage} contentFit="cover" />
+    <Image
+      style={styles.personImage}
+      source={{
+        uri: 'https://raw.githubusercontent.com/mAdutskevich/lottery-test/eea563e4d621e2e1eb279ef4f69dcc42176157e0/src/assets/png/lady.png',
+      }}
+      contentFit="cover"
+    />
 
     <Image
       style={styles.lottoImage}
-      source={LottoLogoImage}
+      source={{
+        uri: 'https://raw.githubusercontent.com/mAdutskevich/lottery-test/eea563e4d621e2e1eb279ef4f69dcc42176157e0/src/assets/png/lottoLogo.png',
+      }}
       contentFit="cover"
     />
 
